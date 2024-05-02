5e1cd8e49f767542924ca955d118e4004cf96a77
HasanCoder66 <hasanashraf874@gmail.com>
2024-05-02 20:43:57 +0500

    not solve yet

--- BACKEND/Routes/uploadRoute.js
+++ BACKEND/Routes/uploadRoute.js
@@ -1,29 +1,29 @@
 import express from "express";
 import multer from 'multer'
-import { v2 as cloudinary } from "cloudinary";
+import  cloudinary  from "cloudinary";
 import { uploadImages } from "../Controllers/uploadController.js";
 import  dotenv  from "dotenv";
 
 dotenv.config();
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
 
-console.log(cloudinary.config());
+// console.log(cloudinary.config());
 
 // export default cloudinary
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      return cb(null, "uploads"); // Removed leading "/"
+      return cb(null, "uploads/"); // Removed leading "/"
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
       console.log('file',file, uniqueSuffix)
-      cb(null, file.originalname); // Added "-" separator
+      cb(null, `${uniqueSuffix}-${file.originalname}`); // Added "-" separator
     },
   });
   
