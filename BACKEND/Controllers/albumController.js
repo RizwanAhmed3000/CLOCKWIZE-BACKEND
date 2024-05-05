290cc8af199b88ca56406600a16fe5028dfe707d
HasanCoder66 <hasanashraf874@gmail.com>
2024-05-05 21:51:49 +0500

    get all album 1 api remain on this crud for album

--- BACKEND/Controllers/albumController.js
+++ BACKEND/Controllers/albumController.js
@@ -1,6 +1,6 @@
 import photos from "../Models/PhotosModel.js";
 
-//====================  NEW RESIDENT =========================//
+//====================  NEW ALBUM =========================//
 // http://localhost:8800/api/album/
 export const createAlbum = async (req, res, next) => {
     try {
@@ -9,10 +9,10 @@ export const createAlbum = async (req, res, next) => {
       // Create a new Family object with the resident ID from the request parameters
       const newAlbum = new photos({
         ...req.body,
-        residentId: residentId, // Set the residentId property of the Family object
+        residentId: residentId, // Set the residentId property of the Photo object
       });
   
-      // Save the new family member to the database
+      // Save the new album to the database
       const saveAlbum = await newAlbum.save();
       // console.log(saveFamily)
       const { _id } = saveAlbum;
@@ -36,12 +36,12 @@ export const createAlbum = async (req, res, next) => {
   };
 // };
 
-// //UPDATE RESIDENT
-// http://localhost:8800/api/resident/660b37d3da1211544662db30
-export const updateResident = async (req, res, next) => {
+// //UPDATE ALBUM
+// http://localhost:8800/api/album/update/660b37d3da1211544662db30
+export const updateAlbum = async (req, res, next) => {
   try {
-    const updateResident = await photos.findByIdAndUpdate(
-      req.params.residentId,
+    const updateAlbum = await photos.findByIdAndUpdate(
+      req.params.albumId,
       {
         $set: req.body,
       },
@@ -49,8 +49,8 @@ export const updateResident = async (req, res, next) => {
     );
     res.status(200).send({
       status: "Successful",
-      message: "Resident Updated Successfully",
-      data: updateResident,
+      message: "Album Updated Successfully",
+      data: updateAlbum,
     });
   } catch (error) {
     next(error);
@@ -58,35 +58,35 @@ export const updateResident = async (req, res, next) => {
 };
 
 // //DELETE RESIDENT
-// http://localhost:8800/api/resident/660b37d3da1211544662db30
+// http://localhost:8800/api/album/delete/660b37d3da1211544662db30
 
-// export const deleteResident = async (req, res, next) => {
-//   try {
-//     await photos.findByIdAndDelete(req.params.residentId);
-//     res.status(200).send({
-//       status: "Successful",
-//       message: "Resident deleted Successfully",
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-// //GET RESIDENT
-// http://localhost:8800/api/resident/find/660b413793cbd11706eb9a32
-
-export const getResident = async (req, res, next) => {
+export const deleteAlbum = async (req, res, next) => {
   try {
-    const resident = await photos.findById(req.params.residentId);
-    !resident &&
+    await photos.findByIdAndDelete(req.params.albumId);
+    res.status(200).send({
+      status: "Successful",
+      message: "Album deleted Successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// //GET ALBUM
+// http://localhost:8800/api/album/find/660b413793cbd11706eb9a32
+
+export const getAlbum = async (req, res, next) => {
+  try {
+    const album = await photos.findById(req.params.albumId);
+    !album &&
       res.status(404).send({
         status: "Failed",
-        message: "Resident not found",
+        message: "Album not found",
       });
     res.status(200).send({
       status: "Successful",
-      message: "Resident Found",
-      data: resident,
+      message: "Album Found",
+      data: album,
     });
   } catch (error) {
     next(error);
--- BACKEND/Routes/albumRoute.js
+++ BACKEND/Routes/albumRoute.js
@@ -1,18 +1,24 @@
 import express from "express";
 import {
-  createAlbum
+  createAlbum,
+  updateAlbum,
+  deleteAlbum,
+  getAlbum
 } from "../Controllers/albumController.js";
 
 const albumRoutes = express.Router();
 
-//localhost:8800/api/auth/signup
+//localhost:8800/api/album/:residentId
 albumRoutes.post("/:residentId", createAlbum);
 
 
-//localhost:8800/api/auth/login
-// albumRoutes.post("/login", login);
+//localhost:8800/api/album/update/:albumId
+albumRoutes.put("/update/:albumId", updateAlbum);
 
-//localhost:8800/api/auth/forgetpass
-// albumRoutes.get("/forgetPass", forgotPassword);
+//localhost:8800/api/album/delete/:albumId
+albumRoutes.delete("/delete/:albumId", deleteAlbum);
+
+//localhost:8800/api/album/find/:albumId
+albumRoutes.get("/find/:albumId", getAlbum);
 
 export default albumRoutes;
